69234442f0a572d6c116a3472cfde64550af0662
dyn-do <117513135+dyn-do@users.noreply.github.com>
2023-01-24 20:20:52 +0900

    aklsdj

--- src/routes/[...path]/+server.ts
+++ src/routes/[...path]/+server.ts
@@ -1,16 +1,24 @@
 import { error, type RequestHandler } from "@sveltejs/kit";
 import { D1Service as D1Connect } from "../../services/D1Connect";
+import { ErrorUtils } from "../../utils/ErrorUtils";
 
 export const POST: RequestHandler = async ({ request, params, platform }) => {
     const path = params["path"];
     if (platform && path) {
 
-        // run d1 call;
-        const connect = new D1Connect(platform);
-        const res = await connect.fetch(path, request);
-        // CORS 
-        res.headers.append('Access-Control-Allow-Origin', "*");
-        return res;
+        try {
+
+            // run d1 call;
+            const connect = new D1Connect(platform);
+            const res = await connect.fetch(path, request);
+            // CORS 
+            res.headers.append('Access-Control-Allow-Origin', "*");
+            return res;
+        } catch (e) {
+            throw error(500, {
+                message: ErrorUtils.stackTrace(e)
+            });
+        }
     } else {
         throw error(404, {
             message: "No Cloudflare Worker"
