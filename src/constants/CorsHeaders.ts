ed8b14df916253410ba44b2c077ea1517c7681bb
dyn-do <117513135+dyn-do@users.noreply.github.com>
2023-01-28 14:36:53 +0900

    あｓｗｄふぁ

--- src/constants/CorsHeaders.ts
+++ src/constants/CorsHeaders.ts
@@ -1,7 +1,6 @@
 export const CorsHeaders = {
     'Access-Control-Allow-Credentials': 'true',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'OPTIONS,POST',
-    'Access-Control-Allow-Headers':
-        'authorization, x-client-info, apikey, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Headers': '*',
 } as const;
