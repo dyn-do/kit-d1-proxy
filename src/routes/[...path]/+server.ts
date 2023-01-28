ff633044efb79c47e751c5ccbaa842e9d4672eaa
dyn-do <117513135+dyn-do@users.noreply.github.com>
2023-01-28 14:06:00 +0900

    ｊｓｋぁｊｄｆ

--- src/routes/[...path]/+server.ts
+++ src/routes/[...path]/+server.ts
@@ -13,7 +13,7 @@ export const POST: RequestHandler = async ({ request, params, platform }) => {
                 const d1res = await conn.fetch(path, request);
                 const res = new Response(d1res.body);
                 // CORS 
-                res.headers.append('Access-Control-Allow-Origin', "*");
+                res.headers.append('Access-Control-Allow-Origin', request.headers.get('Origin') ?? "*");
                 // res.headers.append('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
                 // res.headers.append('Access-Control-Allow-Methods', 'POST');
                 return res;
@@ -36,7 +36,7 @@ export const POST: RequestHandler = async ({ request, params, platform }) => {
                 const d1res = await fetch("https://api.cloudflare.com/client/v4/accounts/1f60a750acc192ddfcb4077542357b40/d1/database/345aa71f-4f63-4ccd-8b40-c5794880c7b2/" + path, requestInit);
                 const res = new Response(d1res.body);
                 // CORS 
-                res.headers.append('Access-Control-Allow-Origin', "*");
+                res.headers.append('Access-Control-Allow-Origin', request.headers.get('Origin') ?? "*");
                 // res.headers.append('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
                 // res.headers.append('Access-Control-Allow-Methods', 'POST');
                 return res;
